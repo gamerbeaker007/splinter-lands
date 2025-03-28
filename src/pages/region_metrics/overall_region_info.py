41d57d41f87065a0f83d60a2e47c7491878e74d3
gamerbeaker <gamerbeaker@gmail.com>
2025-03-28 21:01:36 +0100

    work in progress

--- src/pages/region_metrics/overall_region_info.py
+++ src/pages/region_metrics/overall_region_info.py
@@ -3,6 +3,7 @@ import streamlit as st
 from src.static.icons import grain_icon_url, stone_icon_url, wood_icon_url, iron_icon_url, sps_icon_url, \
     research_icon_url, tax_icon_url
 from src.utils.card import create_card, card_style
+from src.utils.large_number_util import format_large_number
 
 icon_map = {
     "GRAIN": grain_icon_url,
@@ -27,11 +28,16 @@ def get_page(df):
 
             total_active_deeds = token_df.loc[token_df.total_harvest_pp > 0]
             active_empty = token_df.loc[(token_df.total_harvest_pp > 0) & (token_df.worksite_type == "")]
+            total_boosted_pp = format_large_number(total_active_deeds.total_harvest_pp.sum())
+            total_raw_pp = format_large_number(total_active_deeds.total_base_pp_after_cap.sum())
 
             card_html = create_card(
                 token,
                 f"Deeds: {token_df.index.size} ({total_active_deeds.index.size} Active)<br>" +
-                f"Active w/o Type: {active_empty.index.size}<br>",
+                f"Active w/o Type: {active_empty.index.size}<br>" +
+                f"RAW PP: {total_raw_pp}<br>" +
+                f"BOOSTED PP: {total_boosted_pp}<br>" +
+                f"",
                 icon_map.get(token)
             )
 
--- src/pages/region_metrics/region_header.py
+++ src/pages/region_metrics/region_header.py
@@ -6,6 +6,6 @@ def get_page(all_region_date_df):
     in_use_amount = all_region_date_df.loc[all_region_date_df.in_use].index.size
     st.markdown(f"""
         ### {round((active_amount / 150000) * 100, 2)} % ({active_amount}) Active based on PP.
-        ### {round((in_use_amount / 150000) * 100, 2)} % ({in_use_amount}) Active based on is use state.
+        ### {round((in_use_amount / 150000) * 100, 2)} % ({in_use_amount}) Active based on in_use state.
         """
     )
