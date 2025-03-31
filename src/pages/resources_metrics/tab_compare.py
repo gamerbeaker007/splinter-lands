8b9b04902d59df401709118486ead3b5947ba7bf
gamerbeaker <gamerbeaker@gmail.com>
2025-03-31 14:33:32 +0200

    Make them appear in columns

--- src/pages/resources_metrics/tab_compare.py
+++ src/pages/resources_metrics/tab_compare.py
@@ -10,14 +10,19 @@ log = logging.getLogger("Tab - production")
 
 def plot_by_group(filtered_df, group_col, label_prefix):
     groups = filtered_df[group_col].unique().tolist()
-    for group in groups:
+    cols = st.columns(3)  # create 3 columns
+
+    for i, group in enumerate(groups):
         temp_df = filtered_df.loc[filtered_df[group_col] == group]
         title = f"{label_prefix}: {group}"
         temp_df = get_per_resource_data(temp_df)
-        if not temp_df.empty:
-            region_graphs.create_pp_per_source_type(temp_df, key=str(group), title=title)
-        else:
-            st.warning("No active deed found")
+
+        col = cols[i % 3]  # rotate through columns
+        with col:
+            if not temp_df.empty:
+                region_graphs.create_pp_per_source_type(temp_df, key=str(group), title=title)
+            else:
+                st.warning("No active deed found")
 
 
 def get_page(filtered_all_data):
